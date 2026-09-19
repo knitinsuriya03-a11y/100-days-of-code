ccdfbe480f6e216438d03f177dca931bc43c6153
NITIN SURIYA K <knitinsuriya03@gmail.com>
2026-09-19 12:16:15 +0530

    Add files via upload

---
+++ day-62-starting-files-coffee-and-wifi/main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField, SelectField
+from wtforms.validators import DataRequired, URL
+import csv
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    url = URLField('location url', validators=[URL()])
+    open_time = StringField("open time", validators=[DataRequired()])
+    close_time = StringField("close time", validators=[DataRequired()])
+    coffee = SelectField("coffee rating", choices=(("☕", "☕"), ("☕☕", "☕☕"), ("☕☕☕", "☕☕☕"),
+                                                   ("☕☕☕☕", "☕☕☕☕"), ("☕☕☕☕☕", "☕☕☕☕☕")))
+    wifi = SelectField("wi-fi rating", choices=(("💪", "💪"), ("💪💪", "💪💪"), ("💪💪💪", "💪💪💪"),
+                                                ("💪💪💪💪", "💪💪💪💪"), ("💪💪💪💪💪💪", "💪💪💪💪💪💪")))
+    power = SelectField("power outlet", choices=(("🔌", "🔌"), ("🔌🔌", "🔌🔌"), ("🔌🔌🔌", "🔌🔌🔌"),
+                                                 ("🔌🔌🔌🔌", "🔌🔌🔌🔌"), ("🔌🔌🔌🔌🔌", "🔌🔌🔌🔌🔌")))
+    submit = SubmitField('Submit')
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=["Post", "Get"])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        print("True")
+        cafe = request.form.get("cafe")
+        url = request.form.get("url")
+        open_time = request.form.get("open_time")
+        close_time = request.form.get("close_time")
+        coffee = request.form.get("coffee")
+        wifi = request.form.get("wifi")
+        power = request.form.get("power")
+        with open("cafe-data.csv", "a", newline='', encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow([cafe, url, open_time, close_time, coffee, wifi, power])
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
